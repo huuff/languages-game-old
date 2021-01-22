e71da880a87a944a62cf468d13ba630431fac536
Haf <haf@protonmail.ch>
2021-01-22 12:55:14 +0100

    added gitignore, using template for sorting

--- fibonacci/test.py
+++ fibonacci/test.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
-import os
 import subprocess
 import unittest
-import configparser
 import sys
 sys.path.append('../lib')
 import testbase
-# TODO: Print folder name
 
 class FibonacciTest(testbase.BaseTest):
     def configure_command_and_get_expected(self, command):
--- sorting/test.py
+++ sorting/test.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
-import os
 import random
 import subprocess
 import unittest
-import configparser
+import sys
+sys.path.append('../lib')
+import testbase
 
-# TODO: Print folder name
 # TODO: Accept newline at end of output?
-
-configFile = 'config'
 
 testCase = random.sample(range(0, 10), 10)
 testExpected = testCase.copy()
@@ -17,23 +15,11 @@ testCase = list(map(str, testCase))
 testCaseString = ' '.join(testCase)
 testExpected = ' '.join(map(str, testExpected))
 
-def buildPath(root, fileName):
-    return root + '/' + fileName
+class SortingTest(testbase.BaseTest):
+    def configure_command_and_get_expected(self, command):
+        command.extend(testCase)
+        return testExpected
 
-class SortingTest(unittest.TestCase):
-    def test_sort(self):
-        config = configparser.ConfigParser()
-        config['Commands'] = {'fileName': '', 'runCommand': ''}
-        configVars = config['Commands']
-        for root, dirs, files in os.walk(".", topdown=True):
-            if configFile in files:
-                config.read(buildPath(root, configFile))
-            if configVars['fileName'] in files:
-                command = [configVars['runCommand'], buildPath(root, configVars['fileName'])]
-                command.extend(testCase)
-                result = subprocess.run(command, stdout=subprocess.PIPE)
-                testActual = result.stdout.decode('utf-8')
-                self.assertEqual(testExpected, testActual)
 
 if __name__ == '__main__':
     unittest.main()

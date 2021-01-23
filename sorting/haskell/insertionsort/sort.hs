18a198222f0956734ece3f58fc330b53f79534d2
Haf <haf@protonmail.ch>
2021-01-23 19:22:34 +0100

    started insertion sort in haskell

---
+++ sorting/haskell/insertionsort/sort.hs
@@ -0,0 +1,10 @@
+import System.Environment
+import System.Exit
+import Data.List (insert, intercalate)
+
+insertionSort :: Ord a => [a] -> [a]
+insertionSort = foldr insert []
+
+main = do args <- getArgs
+          let output = insertionSort $ map (read :: String->Int) args
+          print $ intercalate " " (map (show :: Int->String) output)

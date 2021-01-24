5166c8b9c1f869933b95d4efef5cd6acf3714267
Haf <haf@protonmail.ch>
2021-01-24 20:55:08 +0100

    showing directories on each test

--- lib/testbase.py
+++ lib/testbase.py
@@ -28,16 +28,22 @@ class BaseTest(unittest.TestCase):
                 }
         configVars = config['Commands']
         for root, dirs, files in os.walk(".", topdown=True):
+            print(root)
             if configFile in files:
                 config.read(self.build_path(root, configFile))
             if configVars['fileName'] in files:
                 for test_case, expected in self.test_cases.items():
-                    command = [configVars['runCommand'], self.build_path(root, configVars['fileName'])]
-                    command = self.configure_command(test_case, command)
-                    try:
-                        actual = command.run(int(configVars['timeout']) / 1000)
-                        self.assertEqual(expected, self.sanitize_output(actual))
-                    except subprocess.TimeoutExpired:
-                        print('Timed out!')
+                    self.run_test(root, configVars, test_case, expected)
+
+    def run_test(self, root, configVars, test_case, expected):
+        command = [configVars['runCommand'], self.build_path(root, configVars['fileName'])]
+        command = self.configure_command(test_case, command)
+        try:
+            actual = command.run(int(configVars['timeout']) / 1000)
+            self.assertEqual(expected, self.sanitize_output(actual))
+        except subprocess.TimeoutExpired:
+            print('Timed out!')
+
+
     
     def configure_command(self, test_case, command): pass

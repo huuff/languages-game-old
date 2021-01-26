b46f6594dd37c3394662cad4f061d80bd1cffee4
Haf <haf@protonmail.ch>
2021-01-26 15:39:44 +0100

    using my own recursive method to traverse directories

--- main/lib/testbase.py
+++ main/lib/testbase.py
@@ -3,6 +3,8 @@ import subprocess
 import unittest
 import configparser
 import sys
+import glob
+import copy
 from . import command
 
 config_file = 'config'
@@ -11,9 +13,6 @@ class BaseTest():
     test_class = unittest.TestCase()
     def __init__(self, root_dir):
         self.root_dir = root_dir
-
-    def build_path(self, root, file):
-        return root + '/' + file
 
     def sanitize_output(self, output):
         if isinstance(output, str):
@@ -28,16 +27,29 @@ class BaseTest():
                 'timeout': 10_000,
                 })
         config.add_section('Commands')
-        for root, dirs, files in os.walk(self.root_dir, topdown=True):
-            print(root)
-            if config_file in files:
-                config.read(self.build_path(root, config_file))
-            if config.get('Commands', 'file') in files:
-                for test_case, expected in self.test_cases().items():
-                    self.run_test(root, config, test_case, expected)
+        self.recursive_descent(self.root_dir, config)
 
-    def run_test(self, root, config, test_case, expected):
-        command = [config.get('Commands', 'run'), self.build_path(root, config.get('Commands','file'))]
+    def recursive_descent(self, root, config):
+        files = glob.glob(f"{root}/*")
+        config_file = f"{root}/config"
+        test_file = f"{root}/{config.get('Commands', 'file')}"
+        if config_file in files:
+            print(f"Reading {config_file}")
+            config = copy.copy(config)
+            config.read(f"{config_file}")
+        for file in files:
+            if os.path.isdir(file):
+                print(f'Going down {file}')
+                self.recursive_descent(file, config)
+            if file == test_file:
+                print(f"Running script at {test_file}")
+                for test_case, expected in self.test_cases().items():
+                    self.run_test(test_file, config, test_case, expected)
+
+
+
+    def run_test(self, test_file, config, test_case, expected):
+        command = [config.get('Commands', 'run'), test_file]
         command = self.configure_command(test_case, command)
         try:
             actual = command.run(int(config.get('Commands','timeout')) / 1000)
@@ -45,8 +57,8 @@ class BaseTest():
         except subprocess.TimeoutExpired:
             print('Timed out!')
 
-    def test_cases(self):
+    def test_cases(self): # to be implemented in base class
         return {}
     
-    def configure_command(self, test_case, command):
+    def configure_command(self, test_case, command): # to be implemented in base class
         return command.Command()
